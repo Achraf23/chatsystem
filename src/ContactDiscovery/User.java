a8148fc10a0ac322353639d1bde0ddaab018bc4d
Bensebaa <achrafbensebaa23@gmail.com>
2023-11-16 19:12:26 +0100

    on commence les tests

--- src/ContactDiscovery/User.java
+++ src/ContactDiscovery/User.java
@@ -10,7 +10,7 @@ public class User {
 
     ContactList contactList;
     //Récuperer l'adresse IP par un getter
-    private String ip;
+    private final String ip;
     //Pseudo a même à changer
     public String nickname = null;
 
---
+++ src/test/TestContactList.java
@@ -0,0 +1,37 @@
+
+import ContactDiscovery.ContactList;
+import ContactDiscovery.PseudoIP;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+
+public class TestContactList {
+
+    ArrayList<PseudoIP> contactList;
+    @BeforeAll
+    void initTest(){
+        contactList=ContactList.getInstance().table;
+
+    }
+
+    private int uniqueElement(String pseudo){
+        int count=0;
+        for(int i=0;i<contactList.size();i++){
+            if(pseudo.equals(contactList.get(i).pseudo)){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    @Test
+    void isContactListRight(){
+        for(int i=0;i<contactList.size();i++){
+            assertEquals(1,uniqueElement(contactList.get(i).pseudo));
+        }
+    }
+
+
+}
